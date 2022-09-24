27d242453c4a7741583f7135e1ea13c31b37d4d4
Adanck <adantalavera94@gmail.com>
2022-09-24 17:45:48 -0600

    base cs

---
+++ Asoeco/DataAccess/Base.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Asoeco.DataAccess
+{
+    public class Base
+    {
+        SqliteConnection conn = new SqliteConnection("Data Source=.\AsoecoDb.db;Version=3;");
+
+    }
+}
