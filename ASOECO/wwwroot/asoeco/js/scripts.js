bebe2540ecc94abbd80ccb469a03f8a23c9e9b18
Adanck <adantalavera94@gmail.com>
2022-11-15 23:34:21 -0600

    load images

--- ASOECO/wwwroot/asoeco/js/scripts.js
+++ ASOECO/wwwroot/asoeco/js/scripts.js
@@ -1,54 +1,71 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+    //// Navbar shrink function
+    //var navbarShrink = function () {
+    //    const navbarCollapsible = document.body.querySelector('#mainNav');
+    //    if (!navbarCollapsible) {
+    //        return;
+    //    }
+    //    if (window.scrollY === 0) {
+    //        navbarCollapsible.classList.remove('navbar-shrink')
+    //    } else {
+    //        navbarCollapsible.classList.add('navbar-shrink')
+    //    }
 
-    };
+    //};
 
-    // Shrink the navbar 
-    navbarShrink();
+    //// Shrink the navbar 
+    //navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    //// Shrink the navbar when page is scrolled
+    //document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
+    //// Activate Bootstrap scrollspy on the main nav element
+    //const mainNav = document.body.querySelector('#mainNav');
+    //if (mainNav) {
+    //    new bootstrap.ScrollSpy(document.body, {
+    //        target: '#mainNav',
+    //        offset: 72,
+    //    });
+    //};
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+    //// Collapse responsive navbar when toggler is visible
+    //const navbarToggler = document.body.querySelector('.navbar-toggler');
+    //const responsiveNavItems = [].slice.call(
+    //    document.querySelectorAll('#navbarResponsive .nav-link')
+    //);
+    //responsiveNavItems.map(function (responsiveNavItem) {
+    //    responsiveNavItem.addEventListener('click', () => {
+    //        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+    //            navbarToggler.click();
+    //        }
+    //    });
+    //});
 
+    pasasrImagenes();
 });
+
+
+let identificadorIntervaloDeTiempo;
+let contador = 0;
+
+function pasasrImagenes() {
+    debugger
+    identificadorIntervaloDeTiempo = setInterval(pasador, 1000);
+}
+
+function pasador() {
+    debugger;
+    let socios = document.querySelectorAll('[name="filterImg"]');
+    debugger
+    socios = Array.from(socios);
+    contador = socios.length;
+    debugger
+    socios[contador].click();
+    contador++;
+    if (contador > socios.length) {
+        contador = 0;     
+    }
+}
